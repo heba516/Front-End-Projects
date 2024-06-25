da1fcb745f2a6992e3cb4cccfaeafcc143254e1e
Heba Yasser <hebay99@gmail.com>
2024-06-25 03:30:25 +0300

    first

--- first_project/main.js
+++ first_project/main.js
@@ -3,11 +3,11 @@ let img = document.querySelector('.icon_mode img');
 
 img.addEventListener("click", ()=> {
     if (mode.classList.contains('dark')) {
-        img.src = '/images/twemoji--sun.png';
+        img.src = 'images/twemoji--sun.png';
         mode.classList.remove('dark');
         mode.classList.add('light');
     }else {
-        img.src = '/images/icon-park--moon.png';
+        img.src = 'images/icon-park--moon.png';
         mode.classList.remove('light');
         mode.classList.add('dark');
     }
